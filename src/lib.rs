7d75e2a7b3f3c3e6660c39992799c30c9fc9f862
Juan Lledo <juan.lledo@hotmail.com>
2021-09-24 17:50:03 +0200

    Init log before everything else

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,10 @@ use crate::config::Config;
 
 pub mod config;
 mod control;
-mod log;
+pub mod log;
 mod shutdown;
 
 pub fn run(config: Config) -> std::io::Result<Server> {
-    log::init();
     control::run_server(config.control_server.socket_addrs);
     shutdown::run_server(config.shutdown_server.socket_addrs)
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use remote_control::config::Config;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    remote_control::log::init();
     let config = Config::new();
+
     remote_control::run(config)?.await
 }
